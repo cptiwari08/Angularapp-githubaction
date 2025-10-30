3e933b104d3c53ff5e99e246e5ca9b8dbe897129
ChandrapTiwari <iamchandantiwari008@gmail.com>
2025-10-30 17:49:22 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Github Actions</h1>
+      <h2>Complete course with 2 projects heloo sonar scan</h2>
+
+    </>
+  )
+}
+
+export default App
